57e0a5aeb3e0b7543444cc111880026ab2ca1218
dev-oswld <user.oswld@gmail.com>
2021-03-23 23:53:52 -0600

    Colors, font size and timer

--- python/__main__.py
+++ python/__main__.py
@@ -1,6 +1,9 @@
-import multiprocessing as mp
+from tkinter import BOTH, CENTER, NW, SUNKEN, Canvas, Frame, Label, StringVar, Tk, W, messagebox
+from timeit import default_timer as timer
+from datetime import timedelta
 import threading as threading
-
+import multiprocessing as mp
+from route import Route
 from city import City
 from population_utils import (
     _swap,
@@ -11,7 +14,6 @@ from population_utils import (
     partially_mapped_crossover,
     select_parents,
 )
-from route import Route
 
 window_width = 1000
 window_height = 800
@@ -20,7 +22,7 @@ population_size = 100
 parents_size = 50
 max_generations = 500
 
-window_padding = 20
+window_padding = 10
 current_generation = 0
 cities = generate_cities(
     n_cities,
@@ -30,10 +32,6 @@ cities = generate_cities(
     maxy=window_height - window_padding,
 )
 population = generate_population(cities, population_size)
-
-
-from tkinter import BOTH, CENTER, NW, SUNKEN, Canvas, Frame, Label, StringVar, Tk, W
-
 
 class CityMapper(Frame):
     def __init__(self):
@@ -48,12 +46,17 @@ class CityMapper(Frame):
         )
 
     def draw_city(self, x, y, name, **kwargs):
+        '''
+        Pokemon colors 
+        Yellow: #ffde00, Cerulean Blue: #3b4cca, Dark blue: #003a70
+        '''
+
         r = 5
         self.canvas.create_oval(
-            x - r, y - r, x + r, y + r, fill="blue", outline="#DDD", width=4
+            x - r, y - r, x + r, y + r, fill="#003a70", outline="#3b4cca", width=3
         )
         self.canvas.create_text(
-            x, y - 10, anchor=CENTER, font=("Open Sans", 10), text=name
+            x, y - 10, anchor=CENTER, font=("Open Sans", 9), text=name
         )
 
     def draw_best_ui(self, cities, route, generation):
@@ -61,14 +64,14 @@ class CityMapper(Frame):
         self.canvas.delete("all")
 
         self.canvas.create_text(
-            0,
+            5,
             0,
             anchor=NW,
-            font=("Open Sans", 35),
-            text=f"Generation {generation:03} - Distance: {route.distance:.3f}",
+            font=("Open Sans", 18),
+            text=f"Generation {generation:03} \nDistance: {route.distance:.3f}",
         )
         for a, b in zip(route[1:], route[:-1]):
-            self.canvas.create_line(a.x, a.y, b.x, b.y)
+            self.canvas.create_line(a.x, a.y, b.x, b.y, fill="#ffde00", width=3)
         for city in cities:
             self.draw_city(city.x, city.y, city.name)
         self.canvas.pack(fill=BOTH, expand=1)
@@ -77,6 +80,9 @@ class CityMapper(Frame):
 def genetic_algorithm(example, root):
     global population
     global current_generation
+
+    start_time = timer()
+    
     best_routes = sorted(population, key=lambda route: route.distance)
 
     example.draw_best_ui(cities, best_routes[0], current_generation)
@@ -88,7 +94,10 @@ def genetic_algorithm(example, root):
     population = parents + new_children_mutated
 
     if current_generation == max_generations:
-        print("Done!")
+        end_time = timer()
+        final_time = end_time - start_time
+        message = "Current generation " + str(current_generation) + "\n Time {}".format(timedelta(seconds=final_time))
+        messagebox.showinfo("Done!", message)
     else:
         root.after(100, genetic_algorithm, example, root)
     current_generation = current_generation + 1
@@ -97,7 +106,6 @@ def genetic_algorithm(example, root):
 def main():
 
     root = Tk()
-
     app = CityMapper()
     root.geometry(f"{window_width}x{window_height}")
     root.after(100, genetic_algorithm, app, root)
